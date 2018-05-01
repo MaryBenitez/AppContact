197792850443e95385487cb8a5a45bae63b78f9f
MaryBenitez <marisol.dominguezxp@gmail.com>
2018-05-01 17:39:56 -0600

    Initial commit, se creó la clase donde estan los atributos del contacto, y el diseño inicial de la app

---
+++ app/src/main/java/com/example/maris/appmaryjencontact/DetalleContacto.java
@@ -0,0 +1,44 @@
+package com.example.maris.appmaryjencontact;
+
+public class DetalleContacto {
+
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private int numero;
+
+    public DetalleContacto(String nombre, String apellido, String direccion, int numero) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
